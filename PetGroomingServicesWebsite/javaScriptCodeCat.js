3eb11824238813752fa266f81a8c113b22b32740
Raya <110840012+RayaTaji@users.noreply.github.com>
2022-09-04 12:23:58 +0300

    Changed cat service prices
    + some of the responsive elements

--- PetGroomingServicesWebsite/javaScriptCodeCat.js
+++ PetGroomingServicesWebsite/javaScriptCodeCat.js
@@ -30,47 +30,47 @@ function labelChangeSmall() {
     /////////////////////////////////////////
     //grooming price from medium to small and from large to small  
     if (l1.innerHTML === "Grooming") {
-        l1.innerHTML = "Grooming: 6 JD"
+        l1.innerHTML = "Grooming: 3 JD"
     }
-    else if (l1.innerHTML === "Grooming: 8 JD") {
-        l1.innerHTML = "Grooming: 6 JD"
+    else if (l1.innerHTML === "Grooming: 5 JD") {
+        l1.innerHTML = "Grooming: 3 JD"
     }
-    else if (l1.innerHTML === "Grooming: 10 JD") {
-        l1.innerHTML = "Grooming: 6 JD"
+    else if (l1.innerHTML === "Grooming: 7 JD") {
+        l1.innerHTML = "Grooming: 3 JD"
     }
     /////////////////////////////////////////
     //bathing price from medium to small and from large to small  
     if (l2.innerHTML === "Bathing") {
-        l2.innerHTML = "Bathing: 10 JD"
+        l2.innerHTML = "Bathing: 5 JD"
     }
-    else if (l2.innerHTML === "Bathing: 15 JD") {
-        l2.innerHTML = "Bathing: 10 JD"
+    else if (l2.innerHTML === "Bathing: 7 JD") {
+        l2.innerHTML = "Bathing: 5 JD"
     }
-    else if (l2.innerHTML === "Bathing: 20 JD") {
-        l2.innerHTML = "Bathing: 10 JD"
+    else if (l2.innerHTML === "Bathing: 9 JD") {
+        l2.innerHTML = "Bathing: 5 JD"
     }
     /////////////////////////////////////////
     //trimming price from medium to small and from large to small  
     if (l3.innerHTML === "Basic Trimming") {
-        l3.innerHTML = "Basic Trimming: 10 JD"
+        l3.innerHTML = "Basic Trimming: 3 JD"
     }
-    else if (l3.innerHTML === "Basic Trimming: 15 JD") {
-        l3.innerHTML = "Basic Trimming: 10 JD"
+    else if (l3.innerHTML === "Basic Trimming: 5 JD") {
+        l3.innerHTML = "Basic Trimming: 3 JD"
     }
-    else if (l3.innerHTML === "Basic Trimming: 20 JD") {
-        l3.innerHTML = "Basic Trimming: 10 JD"
+    else if (l3.innerHTML === "Basic Trimming: 7 JD") {
+        l3.innerHTML = "Basic Trimming: 3 JD"
     }
     /////////////////////////////////////////
     //hairstyle price from medium to small and from large to small 
     if (l4.innerHTML === "Hair Styling") {
-        l4.innerHTML = "Hair Styling: 15 JD"
+        l4.innerHTML = "Hair Styling: 4 JD"
     }
-    else if (l4.innerHTML === "Hair Styling: 20 JD") {
-        l4.innerHTML = "Hair Styling: 15 JD"
+    else if (l4.innerHTML === "Hair Styling: 6 JD") {
+        l4.innerHTML = "Hair Styling: 4 JD"
     }
 
-    else if (l4.innerHTML === "Hair Styling: 25 JD") {
-        l4.innerHTML = "Hair Styling: 15 JD"
+    else if (l4.innerHTML === "Hair Styling: 8 JD") {
+        l4.innerHTML = "Hair Styling: 4 JD"
     }
 }
 
@@ -83,47 +83,47 @@ function labelChangeMedium() {
     /////////////////////////////////////////
     //grooming price from small to medium and from large to medium 
     if (l1.innerHTML === "Grooming") {
-        l1.innerHTML = "Grooming: 8 JD"
+        l1.innerHTML = "Grooming: 5 JD"
     }
-    else if (l1.innerHTML === "Grooming: 6 JD") {
-        l1.innerHTML = "Grooming: 8 JD"
+    else if (l1.innerHTML === "Grooming: 3 JD") {
+        l1.innerHTML = "Grooming: 5 JD"
     }
-    else if (l1.innerHTML === "Grooming: 10 JD") {
-        l1.innerHTML = "Grooming: 8 JD"
+    else if (l1.innerHTML === "Grooming: 7 JD") {
+        l1.innerHTML = "Grooming: 5 JD"
     }
     /////////////////////////////////////////
     //bathing price from small to medium and from large to medium 
     if (l2.innerHTML === "Bathing") {
-        l2.innerHTML = "Bathing: 15 JD"
+        l2.innerHTML = "Bathing: 7 JD"
     }
-    else if (l2.innerHTML === "Bathing: 10 JD") {
-        l2.innerHTML = "Bathing: 15 JD"
+    else if (l2.innerHTML === "Bathing: 5 JD") {
+        l2.innerHTML = "Bathing: 7 JD"
     }
 
-    else if (l2.innerHTML === "Bathing: 20 JD") {
-        l2.innerHTML = "Bathing: 15 JD"
+    else if (l2.innerHTML === "Bathing: 9 JD") {
+        l2.innerHTML = "Bathing: 7 JD"
     }
     /////////////////////////////////////////
     //trimming price from small to medium and from large to medium
     if (l3.innerHTML === "Basic Trimming") {
-        l3.innerHTML = "Basic Trimming: 15 JD"
+        l3.innerHTML = "Basic Trimming: 5 JD"
     }
-    else if (l3.innerHTML === "Basic Trimming: 10 JD") {
-        l3.innerHTML = "Basic Trimming: 15 JD"
+    else if (l3.innerHTML === "Basic Trimming: 3 JD") {
+        l3.innerHTML = "Basic Trimming: 5 JD"
     }
-    else if (l3.innerHTML === "Basic Trimming: 20 JD") {
-        l3.innerHTML = "Basic Trimming: 15 JD"
+    else if (l3.innerHTML === "Basic Trimming: 7 JD") {
+        l3.innerHTML = "Basic Trimming: 5 JD"
     }
     /////////////////////////////////////////
     //hairstyle price from small to medium and from large to medium 
     if (l4.innerHTML === "Hair Styling") {
-        l4.innerHTML = "Hair Styling: 20 JD"
+        l4.innerHTML = "Hair Styling: 6 JD"
     }
-    else if (l4.innerHTML === "Hair Styling: 15 JD") {
-        l4.innerHTML = "Hair Styling: 20 JD"
+    else if (l4.innerHTML === "Hair Styling: 4 JD") {
+        l4.innerHTML = "Hair Styling: 6 JD"
     }
-    else if (l4.innerHTML === "Hair Styling: 25 JD") {
-        l4.innerHTML = "Hair Styling: 20 JD"
+    else if (l4.innerHTML === "Hair Styling: 8 JD") {
+        l4.innerHTML = "Hair Styling: 6 JD"
     }
 
 }
@@ -137,46 +137,46 @@ function labelChangeLarge() {
     /////////////////////////////////////////
     //grooming price from small to large and from medium to large  
     if (l1.innerHTML === "Grooming") {
-        l1.innerHTML = "Grooming: 10 JD"
+        l1.innerHTML = "Grooming: 7 JD"
     }
-    else if (l1.innerHTML === "Grooming: 6 JD") {
-        l1.innerHTML = "Grooming: 10 JD"
+    else if (l1.innerHTML === "Grooming: 3 JD") {
+        l1.innerHTML = "Grooming: 7 JD"
     }
-    else if (l1.innerHTML === "Grooming: 8 JD") {
-        l1.innerHTML = "Grooming: 10 JD"
+    else if (l1.innerHTML === "Grooming: 5 JD") {
+        l1.innerHTML = "Grooming: 7 JD"
     }
     /////////////////////////////////////////
     //bathing price from small to large and from medium to large  
     if (l2.innerHTML === "Bathing") {
-        l2.innerHTML = "Bathing: 20 JD"
+        l2.innerHTML = "Bathing: 9 JD"
     }
-    else if (l2.innerHTML === "Bathing: 10 JD") {
-        l2.innerHTML = "Bathing: 20 JD"
+    else if (l2.innerHTML === "Bathing: 5 JD") {
+        l2.innerHTML = "Bathing: 9 JD"
     }
-    else if (l2.innerHTML === "Bathing: 15 JD") {
-        l2.innerHTML = "Bathing: 20 JD"
+    else if (l2.innerHTML === "Bathing: 7 JD") {
+        l2.innerHTML = "Bathing: 9 JD"
     }
     ////////////////////////////////////////////////////////
     //trimming price from small to large and from medium to large 
     if (l3.innerHTML === "Basic Trimming") {
-        l3.innerHTML = "Basic Trimming: 20 JD"
+        l3.innerHTML = "Basic Trimming: 7 JD"
     }
-    else if (l3.innerHTML === "Basic Trimming: 10 JD") {
-        l3.innerHTML = "Basic Trimming: 20 JD"
+    else if (l3.innerHTML === "Basic Trimming: 3 JD") {
+        l3.innerHTML = "Basic Trimming: 7 JD"
     }
-    else if (l3.innerHTML === "Basic Trimming: 15 JD") {
-        l3.innerHTML = "Basic Trimming: 20 JD"
+    else if (l3.innerHTML === "Basic Trimming: 5 JD") {
+        l3.innerHTML = "Basic Trimming: 7 JD"
     }
     ////////////////////////////////////////////////////////
     //Hair Styling price from small to large and from medium to large 
     if (l4.innerHTML === "Hair Styling") {
-        l4.innerHTML = "Hair Styling: 25 JD"
+        l4.innerHTML = "Hair Styling: 8 JD"
     }
-    else if (l4.innerHTML === "Hair Styling: 15 JD") {
-        l4.innerHTML = "Hair Styling: 25 JD"
+    else if (l4.innerHTML === "Hair Styling: 4 JD") {
+        l4.innerHTML = "Hair Styling: 8 JD"
     }
-    else if (l4.innerHTML === "Hair Styling: 20 JD") {
-        l4.innerHTML = "Hair Styling: 25 JD"
+    else if (l4.innerHTML === "Hair Styling: 6 JD") {
+        l4.innerHTML = "Hair Styling: 8 JD"
     }
 
 }
